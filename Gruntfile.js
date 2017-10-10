1b3ac7e205b8f3bec23a222ef80bc655718bca5a
Tim Beadle <tim@takkconsulting.co.uk>
2017-10-10 21:43:34 +0100

    tabs -> spaces

--- Gruntfile.js
+++ Gruntfile.js
@@ -8,76 +8,69 @@
 
 /*global module, require */
 
-module.exports = function (grunt) {
+module.exports = function(grunt) {
+  "use strict";
 
-	'use strict';
+  // load all npm grunt tasks
+  require("load-grunt-tasks")(grunt);
 
-	// load all npm grunt tasks
-	require('load-grunt-tasks')(grunt);
+  // Project configuration.
+  grunt.initConfig({
+    eslint: {
+      src: ["Gruntfile.js", "tasks/*.js", "<%= nodeunit.tests %>"],
+      options: {
+        configFile: ".eslintrc.yml"
+      }
+    },
 
-	// Project configuration.
-	grunt.initConfig({
-		eslint: {
-			src: [
-				'Gruntfile.js',
-				'tasks/*.js',
-				'<%= nodeunit.tests %>'
-			],
-			options: {
-				configFile: '.eslintrc.yml'
-			}
-		},
+    "release-it": {
+      options: {
+        pkgFiles: ["package.json"],
+        commitMessage: "Release %s",
+        tagName: "%s",
+        tagAnnotation: "Release %s",
+        buildCommand: false
+      }
+    },
 
-		'release-it': {
-			options: {
-				pkgFiles: ['package.json'],
-				commitMessage: 'Release %s',
-				tagName: '%s',
-				tagAnnotation: 'Release %s',
-				buildCommand: false
-			}
-		},
+    // Before generating any new files, remove any previously-created files.
+    clean: {
+      tests: ["tmp"]
+    },
 
-		// Before generating any new files, remove any previously-created files.
-		clean: {
-			tests: ['tmp']
-		},
+    // Configuration to be run (and then tested).
+    cfpathcheck: {
+      default_options: {
+        options: {},
+        files: {
+          "tmp/default_options": ["test/fixtures/testing", "test/fixtures/123"]
+        }
+      },
+      custom_options: {
+        options: {
+          separator: ": ",
+          punctuation: " !!!"
+        },
+        files: {
+          "tmp/custom_options": ["test/fixtures/testing", "test/fixtures/123"]
+        }
+      }
+    },
 
-		// Configuration to be run (and then tested).
-		cfpathcheck: {
-			default_options: {
-				options: {},
-				files: {
-					'tmp/default_options': ['test/fixtures/testing', 'test/fixtures/123']
-				}
-			},
-			custom_options: {
-				options: {
-					separator: ': ',
-					punctuation: ' !!!'
-				},
-				files: {
-					'tmp/custom_options': ['test/fixtures/testing', 'test/fixtures/123']
-				}
-			}
-		},
+    // Unit tests.
+    nodeunit: {
+      tests: ["test/*_test.js"]
+    }
+  });
 
-		// Unit tests.
-		nodeunit: {
-			tests: ['test/*_test.js']
-		}
+  // Actually load this plugin's task(s).
+  grunt.loadTasks("tasks");
 
-	});
+  // Whenever the "test" task is run, first clean the "tmp" dir, then run this
+  // plugin's task(s), then test the result.
+  // grunt.registerTask('test', ['clean', 'cfpathcheck', 'nodeunit']);
+  grunt.registerTask("test", ["eslint"]);
 
-	// Actually load this plugin's task(s).
-	grunt.loadTasks('tasks');
-
-	// Whenever the "test" task is run, first clean the "tmp" dir, then run this
-	// plugin's task(s), then test the result.
-	// grunt.registerTask('test', ['clean', 'cfpathcheck', 'nodeunit']);
-	grunt.registerTask('test', ['eslint']);
-
-	// By default, lint and run all tests.
-	grunt.registerTask('default', ['test']);
-
+  // By default, lint and run all tests.
+  grunt.registerTask("default", ["test"]);
 };
--- tasks/cfpathcheck.js
+++ tasks/cfpathcheck.js
@@ -8,41 +8,43 @@
 
 /*global module, require */
 
-module.exports = function (grunt) {
+module.exports = function(grunt) {
+  "use strict";
 
-	'use strict';
+  // Please see the Grunt documentation for more information regarding task
+  // creation: http://gruntjs.com/creating-tasks
 
-	// Please see the Grunt documentation for more information regarding task
-	// creation: http://gruntjs.com/creating-tasks
+  grunt.registerMultiTask(
+    "cfpathcheck",
+    "Grunt wrapper for cfpathcheck",
+    function() {
+      var cfpathcheck = require("cfpathcheck/lib/cfpathcheck");
 
-	grunt.registerMultiTask('cfpathcheck', 'Grunt wrapper for cfpathcheck', function () {
+      // Merge task-specific and/or target-specific options with these defaults.
+      var options = this.options({
+        reporter: "console",
+        outFile: undefined
+      });
+      var file;
 
-		var cfpathcheck = require('cfpathcheck/lib/cfpathcheck');
+      if (this.filesSrc.length === 1) {
+        file = this.filesSrc[0];
+      }
 
-		// Merge task-specific and/or target-specific options with these defaults.
-		var options = this.options({
-			reporter: 'console',
-			outFile: undefined
-		});
-		var file;
+      // Iterate over all specified file groups.
+      if (typeof file !== "undefined") {
+        var violations = cfpathcheck.check(file, "json");
+        var output = cfpathcheck.formatter(violations, options.reporter);
 
-		if (this.filesSrc.length === 1) {
-			file = this.filesSrc[0];
-		}
+        cfpathcheck.writeOutput(output);
 
-		// Iterate over all specified file groups.
-		if (typeof file !== 'undefined') {
-			var violations = cfpathcheck.check(file, 'json');
-			var output = cfpathcheck.formatter(violations, options.reporter);
-
-			cfpathcheck.writeOutput(output);
-
-			if (options.outFile) {
-				cfpathcheck.writeFile(cfpathcheck.formatter(violations, 'checkstyle'), options.outFile);
-			}
-
-
-		}
-	});
-
+        if (options.outFile) {
+          cfpathcheck.writeFile(
+            cfpathcheck.formatter(violations, "checkstyle"),
+            options.outFile
+          );
+        }
+      }
+    }
+  );
 };
