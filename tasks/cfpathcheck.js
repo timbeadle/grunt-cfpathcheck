9b0dec05e9fd666e44ad84039c9a43a26b72fa95
Tim Beadle <tim@takkconsulting.co.uk>
2022-06-13 19:43:53 +0100

    Do all task functionailty inside the async IIFE

--- tasks/cfpathcheck.js
+++ tasks/cfpathcheck.js
@@ -12,46 +12,45 @@ module.exports = function (grunt) {
 	// Please see the Grunt documentation for more information regarding task
 	// creation: http://gruntjs.com/creating-tasks
 
-	let cfpathcheck = {};
-
 	grunt.registerMultiTask(
 		'cfpathcheck',
 		'Grunt wrapper for cfpathcheck',
 		function () {
-			(async function () {
-				cfpathcheck = {
-					check: (await import('cfpathcheck/lib/cfpathcheck')).check,
-					formatter: (await import('cfpathcheck/lib/cfpathcheck')).formatter,
-					writeFile: (await import('cfpathcheck/lib/cfpathcheck')).writeFile,
-					writeOutput: (await import('cfpathcheck/lib/cfpathcheck')).writeOutput,
-				};
-			})();
-
 			// Merge task-specific and/or target-specific options with these defaults.
 			const options = this.options({
 				reporter: 'console',
 				outFile: undefined,
 			});
+
 			let file;
 
 			if (this.filesSrc.length === 1) {
 				file = this.filesSrc[0];
 			}
 
-			// Iterate over all specified file groups.
-			if (typeof file !== 'undefined') {
-				const violations = cfpathcheck.check(file, 'json');
-				const output = cfpathcheck.formatter(violations, options.reporter);
+			const done = this.async();
 
-				cfpathcheck.writeOutput(output);
+			(async function () {
+				// @see https://dev.to/adamcoster/commonjs-and-esm-importexport-compatibility-by-simple-example-50pl
+				const cfpathcheck = (await import('cfpathcheck/lib/cfpathcheck.js'));
 
-				if (options.outFile) {
-					cfpathcheck.writeFile(
-						cfpathcheck.formatter(violations, 'checkstyle'),
-						options.outFile,
-					);
+				// Iterate over all specified file groups.
+				if (typeof file !== 'undefined') {
+					const violations = cfpathcheck.check(file, 'json');
+					const output = cfpathcheck.formatter(violations, options.reporter);
+
+					cfpathcheck.writeOutput(output);
+
+					if (options.outFile) {
+						cfpathcheck.writeFile(
+							cfpathcheck.formatter(violations, 'checkstyle'),
+							options.outFile,
+						);
+					}
 				}
-			}
+
+				done(true);
+			})();
 		},
 	);
 };
